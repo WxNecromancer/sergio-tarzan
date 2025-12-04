287317afc019f133494abb22be100448e3a2ab99
WxNecromancer <69096838+WxNecromancer@users.noreply.github.com>
2025-12-04 10:17:04 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-  std::cout << "Said llego con un gato con mano amarilla de 4 dedos :)" << std::endl;
+  std::cout << "Said llego con un gato con mano amarilla de 4 dedos :)." << std::endl;
 // OTRO CODIO como de pruebas, actividades extras, etc.
   return 0;
 }
