893810a384fa1da6107e65d1ed5d6f24dc573b0a
WxNecromancer <baltazar.mtz.galla@gmail.com>
2025-12-04 10:24:55 -0600

    Anexo de otra linea de mensaje

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main(){
   std::cout << "Said llego con un gato con mano amarilla de 4 dedos :)." << std::endl;
+  std::cout << "Lo que dijo Alan Albino es correcto, la cajetie con los espacio y guiones" << std::endl;
 // OTRO CODIO como de pruebas, actividades extras, etc.
   return 0;
 }
