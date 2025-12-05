f82ca6ff7406c1eb37d72e31792e3d5caaa7e071
WxNecromancer <69096838+WxNecromancer@users.noreply.github.com>
2025-12-05 07:09:42 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 int main(){
   std::cout << "Said llego con un gato con mano amarilla de 4 dedos :)." << std::endl;
   std::cout << "Lo que dijo Alan Albino es correcto, la cajetie con los espacio y guiones" << std::endl;
+  std::cout << "Christian dice Nice!!!" << std::endl;
+  std::cout << "Karol dijo hola en voz bajita" << std::endl;
 // OTRO CODIO como de pruebas, actividades extras, etc.
   return 0;
 }
